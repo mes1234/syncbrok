477760312201db542acfa43b2571ce24f47d3876
WK WK <witold.kepczynski@gmail.com>
2021-04-10 21:27:24 +0200

    added first uint test

---
+++ internal/msg/simple_test.go
@@ -0,0 +1,16 @@
+package msg_test
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/mes1234/syncbrok/internal/msg"
+)
+
+func TestNewSimpleMsgCreation(t *testing.T) {
+	got := msg.NewSimpleMsg(uuid.Nil, nil)
+
+	if got.GetId() == uuid.Nil {
+		t.Errorf("Gid shall be assigned for evey msg %v", got)
+	}
+}
